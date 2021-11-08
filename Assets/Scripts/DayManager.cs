f45318509099d9a51d9ae0f92e5a22e69f526497
Jean-Théophane <jtdemulatier@gmail.com>
2021-11-08 18:00:57 -0500

    Ajout d'un système de jour visible dans le jeu

--- Assets/Scripts/DayManager.cs
+++ Assets/Scripts/DayManager.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DayManager : MonoBehaviour
 {
     private QueueAction queue;
     public int nDays;
+    public TextMeshProUGUI textmeshPro;
 
     // Start is called before the first frame update
     void Start()
     {
         this.nDays = 0;
         this.queue = new QueueAction();
-
+        TextMeshProUGUI textmeshPro = this.GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void passDay()
     {
         this.nDays++;
         Dictionary<string, int> rewardsFromTheDay = queue.doAllActions();
-        Debug.Log(nDays);
+        // Debug.Log(nDays);
+        textmeshPro.SetText("Days : {0}.", this.nDays);
+
     }
 
 }
