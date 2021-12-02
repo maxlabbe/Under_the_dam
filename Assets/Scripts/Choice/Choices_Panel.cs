bace43cecbcd5ac24367c2f51c6dc59168cf13c6
Jean-Théophane <jtdemulatier@gmail.com>
2021-12-02 02:58:22 -0500

    Les choix sont paramétrés et sont responsiv en fonction de la tile

--- Assets/Scripts/Choice/Choices_Panel.cs
+++ Assets/Scripts/Choice/Choices_Panel.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using Newtonsoft.Json.Linq;
 using TMPro;
 
 public class Choices_Panel : MonoBehaviour
@@ -12,31 +14,69 @@ public class Choices_Panel : MonoBehaviour
     public GameObject toggle;
     public GameObject choices_list;
     public ToggleGroup toggle_list;
+    private dynamic data;
 
     // Start is called before the first frame update
     void Start()
     {
-        title.SetText("VOUS AVEZ LE CHOIX");
-        description.SetText("Decidez de ce que vous allez faire");
+        string json = File.ReadAllText(Application.dataPath + "/data/action.json");
+        data = JObject.Parse(json);
+    }
+
+    public void setAllChoices(string type) {
+        foreach (Transform child in this.toggle_list.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+            GameObject.Destroy(child);
+        }
+        foreach (Transform child in this.choices_list.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+            GameObject.Destroy(child);
+        }
+        string json = File.ReadAllText(Application.dataPath + "/data/action.json");
+        data = JObject.Parse(json);
+        dynamic typedata = data.wood;
+        switch (type)
+        {
+            case "wood":
+                typedata = data.wood;
+                break;
+            case "big_wood":
+                typedata = data.big_wood;
+                break;
+            case "field":
+                typedata = data.field;
+                break;
+            case "big_field":
+                typedata = data.big_field;
+                break;
+            case "village":
+                typedata = data.village;
+                break;
+            default:
+                break;
+        }
+
+        title.SetText((string)typedata.title);
+        description.SetText((string)typedata.description);
         GameObject tmpChoice;
         GameObject tmpToggle;
 
-        tmpChoice = Instantiate(prefab);
-        tmpChoice.transform.parent = choices_list.transform;
-        tmpChoice.GetComponent<AChoice>().setChoice("Attaque!", "Beaver", 10);
-        tmpToggle = Instantiate(toggle);
-        tmpToggle.transform.parent = toggle_list.transform;
+        foreach (dynamic item in typedata.choices)
+        {
+            tmpChoice = Instantiate(prefab);
+            tmpChoice.transform.SetParent(choices_list.transform);
+            tmpChoice.GetComponent<AChoice>().setChoice((string)item.name, ((string)item.needs).Split(':')[0], (int)int.Parse(((string)item.needs).Split(':')[1]));
 
-        tmpChoice = Instantiate(prefab);
-        tmpChoice.transform.parent = choices_list.transform;
-        tmpChoice.GetComponent<AChoice>().setChoice("Récolte!", "Beaver", 5);
-        tmpToggle = Instantiate(toggle);
-        tmpToggle.transform.parent = toggle_list.transform;
+            tmpToggle = Instantiate(toggle);
+            tmpToggle.transform.SetParent(toggle_list.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
