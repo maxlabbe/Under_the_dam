092f8c2bb5e8e6aede57ea7cbcab8a5b6df7e61e
Jean-Théophane <jtdemulatier@gmail.com>
2021-12-03 23:49:21 -0500

    Les choices se mettent et ont été paramétré ainsi qu'une bonne mise en page

--- Assets/Scripts/Choice/AChoice.cs
+++ Assets/Scripts/Choice/AChoice.cs
@@ -8,27 +8,86 @@ public class AChoice : MonoBehaviour
 {
 
     public TextMeshProUGUI nameTextMesh;
-    public TextMeshProUGUI numberTextMesh;
-    public GameObject typeSprite;
+    public GameObject prefabChoosableNeed;
+    public GameObject prefabNonChoosableNeed;
+    public GameObject needsList;
+    public int[] max_needs;
+    public int[] actual_needs;
+    public Sprite wood_Sprite;
+    public Sprite food_Sprite;
+    public Sprite sake_Sprite;
+    public Sprite beaver_Sprite;
+
     // public GameObject nonChoosableNeed;
     // public GameObject choosableNeed;
 
-    public string nameChoice;
-
-    public string typeName;
-    public int currentNumber;
-    public int numberMax;
-
-    public void setChoice(string name, string type, int number_max)
+    public void setChoice(Choice choice)
     {
-        nameChoice = name;
-        typeName = type;
-        numberMax = number_max;
+        max_needs = new int[4];
+        actual_needs = new int[4];
         GameObject nameGameObject = gameObject.transform.Find("name").gameObject;
         nameTextMesh = nameGameObject.GetComponent<TextMeshProUGUI>();
-        // GameObject numberGameObject = gameObject.transform.Find("number").gameObject;
-        // numberTextMesh = numberGameObject.GetComponent<TextMeshProUGUI>();
-        // typeSprite = gameObject.transform.Find("type_of_ressources").gameObject;
+        nameTextMesh.SetText(choice.nameChoice);
+        int i = 0;
+        foreach (Choice.need need in choice.needs)
+        {
+            max_needs[i] = need.value;
+            actual_needs[i] = 0;
+            if (need.value != 0)
+            {
+                if (need.isChoosable)
+                {
+                    GameObject tmpChoosableNeed = Instantiate(prefabChoosableNeed);
+                    tmpChoosableNeed.transform.SetParent(needsList.transform);
+                    Image typeImage = tmpChoosableNeed.transform.Find("arrowAndType").Find("type_of_ressources").gameObject.GetComponent<Image>();
+                    switch (need.type)
+                    {
+                        case "wood":
+                            typeImage.sprite = wood_Sprite;
+                            break;
+                        case "food":
+                            typeImage.sprite = food_Sprite;
+                            break;
+                        case "sake":
+                            typeImage.sprite = sake_Sprite;
+                            break;
+                        case "beaver":
+                            typeImage.sprite = beaver_Sprite;
+                            break;
+                        default:
+                            break;
+                    }
+                    TextMeshProUGUI number = tmpChoosableNeed.transform.Find("number").gameObject.GetComponent<TextMeshProUGUI>();
+                    number.SetText(actual_needs[i].ToString() + " / " + max_needs[i].ToString());
+                }
+                else
+                {
+                    GameObject tmpNonChoosableNeed = Instantiate(prefabNonChoosableNeed);
+                    tmpNonChoosableNeed.transform.SetParent(needsList.transform);
+                    Image typeImage = tmpNonChoosableNeed.transform.Find("arrowAndType").Find("type_of_ressources").gameObject.GetComponent<Image>();
+                    switch (need.type)
+                    {
+                        case "wood":
+                            typeImage.sprite = wood_Sprite;
+                            break;
+                        case "food":
+                            typeImage.sprite = food_Sprite;
+                            break;
+                        case "sake":
+                            typeImage.sprite = sake_Sprite;
+                            break;
+                        case "beaver":
+                            typeImage.sprite = beaver_Sprite;
+                            break;
+                        default:
+                            break;
+                    }
+                    TextMeshProUGUI number = tmpNonChoosableNeed.transform.Find("number").gameObject.GetComponent<TextMeshProUGUI>();
+                    number.SetText(actual_needs[i].ToString() + " / " + max_needs[i].ToString());
+                }
+            }
+            i++;
+        }
     }
 
     // Start is called before the first frame update
@@ -40,25 +99,25 @@ public class AChoice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        nameTextMesh.SetText(nameChoice);
-        switch (typeName)
-        {
-            case "Beaver":
-                typeSprite.GetComponent<Image>().color = Color.blue;
-                break;
-            default:
-                break;
-        }
-        numberTextMesh.SetText(currentNumber.ToString() + " / " + numberMax.ToString());
+        // nameTextMesh.SetText(nameChoice);
+        // switch (typeName)
+        // {
+        //     case "Beaver":
+        //         typeSprite.GetComponent<Image>().color = Color.blue;
+        //         break;
+        //     default:
+        //         break;
+        // }
+        // numberTextMesh.SetText(currentNumber.ToString() + " / " + numberMax.ToString());
     }
 
     public void increment()
     {
-        currentNumber = (currentNumber < numberMax) ? currentNumber + 1 : numberMax;
+        // currentNumber = (currentNumber < numberMax) ? currentNumber + 1 : numberMax;
     }
 
     public void decrement()
     {
-        currentNumber = (currentNumber > 0) ? currentNumber - 1 : 0;
+        // currentNumber = (currentNumber > 0) ? currentNumber - 1 : 0;
     }
 }
--- Assets/Scripts/Choice/Choices_Panel.cs
+++ Assets/Scripts/Choice/Choices_Panel.cs
@@ -14,13 +14,16 @@ public class Choices_Panel : MonoBehaviour
     public GameObject toggle;
     public GameObject choices_list;
     public ToggleGroup toggle_list;
-    private dynamic data;
+    public ChoicesPanelInfos wood;
+    public ChoicesPanelInfos big_wood;
+    public ChoicesPanelInfos field;
+    public ChoicesPanelInfos big_field;
+    public ChoicesPanelInfos village;
 
     // Start is called before the first frame update
     void Start()
     {
-        string json = File.ReadAllText(Application.dataPath + "/data/action.json");
-        data = JObject.Parse(json);
+
     }
 
     public void setAllChoices(string type) {
@@ -34,44 +37,39 @@ public class Choices_Panel : MonoBehaviour
             GameObject.Destroy(child.gameObject);
             GameObject.Destroy(child);
         }
-        string json = File.ReadAllText(Application.dataPath + "/data/action.json");
-        data = JObject.Parse(json);
-        dynamic typedata = data.wood;
+        ChoicesPanelInfos tmp = new ChoicesPanelInfos();
         switch (type)
         {
             case "wood":
-                typedata = data.wood;
+                tmp = wood;
                 break;
             case "big_wood":
-                typedata = data.big_wood;
+                tmp = big_wood;
                 break;
             case "field":
-                typedata = data.field;
+                tmp = field;
                 break;
             case "big_field":
-                typedata = data.big_field;
+                tmp = big_field;
                 break;
             case "village":
-                typedata = data.village;
+                tmp = village;
                 break;
             default:
+                tmp = wood;
                 break;
         }
-
-        title.SetText((string)typedata.title);
-        description.SetText((string)typedata.description);
-        GameObject tmpChoice;
-        GameObject tmpToggle;
-
-        foreach (dynamic item in typedata.choices)
+        title.SetText(tmp.title);
+        description.SetText(tmp.description);
+        foreach (Choice choice in tmp.choices)
         {
-            tmpChoice = Instantiate(prefab);
+            GameObject tmpToogle = Instantiate(toggle);
+            tmpToogle.transform.SetParent(toggle_list.transform);
+            GameObject tmpChoice = Instantiate(prefab);
             tmpChoice.transform.SetParent(choices_list.transform);
-            tmpChoice.GetComponent<AChoice>().setChoice((string)item.name, ((string)item.needs).Split(':')[0], (int)int.Parse(((string)item.needs).Split(':')[1]));
-
-            tmpToggle = Instantiate(toggle);
-            tmpToggle.transform.SetParent(toggle_list.transform);
+            tmpChoice.GetComponent<AChoice>().setChoice(choice);
         }
+
     }
 
     // Update is called once per frame
